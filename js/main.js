562551fdf1b4b0d87c74bb6b028827f53c0e8ba5
griff9 <cgriffith09@gmail.com>
2017-02-20 17:01:27 -0800

    test poke

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var app = {
 
     showAlert: function (message, title) {
-      //  if (navigator.notification) {
-      //      navigator.notification.alert(message, null, title, 'OK');
-      //  } else {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
             alert(title ? (title + ":hokey pokey " + message) : message);
-      //  }
+        }
     },
 
     findByName: function() {
