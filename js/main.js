8c0cdd75cabbd6c3fac4304a77a4af75f952fe30
griff9 <cgriffith09@gmail.com>
2017-02-20 16:59:33 -0800

    comment pokey

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var app = {
       //  if (navigator.notification) {
       //      navigator.notification.alert(message, null, title, 'OK');
       //  } else {
-            alert(title ? (title + ": " + message) : message);
+            alert(title ? (title + ":hokey pokey " + message) : message);
       //  }
     },
 
